ac56a088aff00b8c821b72dd2ec5b43161b87d74
Ibtissam <btissam1234btissam12@gmail.com>
2023-12-13 15:34:57 +0100

    adding authorizationt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Import statements
 import express from "express";
 import cookieParser from "cookie-parser";
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 import cors from "cors";
 import mysql from "mysql";
 import bcrypt from "bcrypt";
@@ -44,26 +44,44 @@ const createUsersTable = () => {
 };
 
 // Register
-app.post('/register',async (req,res)=>{
+app.post('/register', async (req, res) => {
     try {
         const { email, password } = req.body;
-        const  hashpaswword= await bcrypt.hash(password,slt)
-        const data={email:email ,password:hashpaswword}
-        const insertQuery= 'INSERT INTO users SET ?';
-        db.query(insertQuery,data,(err, result) => {
-            if(err) {
-                console.error('Error inserting data into users table:', err);
-               return  res.status(500).json({ Error: 'Internal Server Error' });
+
+        // Check if the email already exists in the database
+        const checkEmailQuery = 'SELECT * FROM users WHERE email = ?';
+        db.query(checkEmailQuery, [email], async (err, result) => {
+            if (err) {
+                console.error('Error checking email in users table:', err);
+                return res.status(500).json({ Error: 'Internal Server Error' });
             }
-            else{
-                console.log("data inset it seccussfully")
-                return res.status(200).json({ Status:"Success"})
+
+            // If the email already exists, return an error response
+            if (result.length > 0) {
+                return res.status(400).json({ Error: 'Email already exists' });
             }
-        })
+
+            // If the email is unique, proceed to insert the new user
+            const hashPassword = await bcrypt.hash(password, slt);
+            const userData = { email: email, password: hashPassword };
+            const insertQuery = 'INSERT INTO users SET ?';
+
+            db.query(insertQuery, userData, (insertErr, insertResult) => {
+                if (insertErr) {
+                    console.error('Error inserting data into users table:', insertErr);
+                    return res.status(500).json({ Error: 'Internal Server Error' });
+                } else {
+                    console.log('Data inserted successfully');
+                    return res.status(200).json({ Status: 'Success' });
+                }
+            });
+        });
     } catch (error) {
-        throw error
+        throw error;
     }
-})
+});
+
+
 // Login
 app.post('/login',async(req,res)=>{
     const getData='SELECT * FROM users WHERE email = ?'
@@ -71,11 +89,12 @@ app.post('/login',async(req,res)=>{
         if(err) return res.json({Error:"Login error in server"});
         if(data.length>0){
             console.log(data);
-            bcrypt.compare(req.body.password,data[0].password,(err,response)=>{
+            bcrypt.compare(req.body.password.toString(),data[0].password,(err,response)=>{
                 if(err) return res.json({Error:"compare password error "});
                 if(response){
                     const email=data[0].email;
                     const token=jwt.sign({email},"jwt secret ",{expiresIn:"1d"});
+                    res.cookie('token',token);
                     return res.status(200).json({ Status:"Success"}) 
                 }else{
                     return res.status(500).json({ Error:"password not match"}) 
@@ -90,8 +109,44 @@ app.post('/login',async(req,res)=>{
     })
 
 })
-// Logout
 
+//authorization
+// verified middleware
+// verified middleware
+const verifyuser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ Error: "you are not authenticated" });
+    } else {
+        jwt.verify(token, "jwt secret ", (err, decoded) => {
+            if (err) {
+                console.error("JWT verification error:", err);
+                return res.json({ Error: "Token verification failed" });
+            } else {
+                // Successful verification, proceed with the logic
+                req.email = decoded.email;
+                next();
+            }
+        });
+        
+    }
+};
+
+app.get('/home', verifyuser, (req, res) => {
+    // Now you can access the authenticated user's email using req.email
+    const userEmail = req.email;
+
+    // Your logic for the home route goes here
+    // For example, you can send a response with the user's email
+    res.status(200).json({ Status: 'Success', Email: userEmail });
+});
+
+// Logout
+app.get('/logout',(req,res)=>{
+    res.clearCookie('token');
+    return res.json({Status:"Success"})
+
+})
 // Server
 app.listen(8000, () => 
 { createUsersTable();
@@ -99,3 +154,24 @@ console.log("Server is running on port 8000")
 });
 
 // Home route
+
+
+
+//delete if i need it
+app.delete('/deleteAllUsers', (req, res) => {
+    try {
+        const deleteQuery = 'DELETE FROM users';
+
+        db.query(deleteQuery, (err, result) => {
+            if (err) {
+                console.error('Error deleting data from users table:', err);
+                return res.status(500).json({ Error: 'Internal Server Error' });
+            } else {
+                console.log('All records deleted from the users table');
+                return res.status(200).json({ Status: 'Success' });
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
+});
